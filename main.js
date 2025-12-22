e3cdf5bf5cf84e7230f8bd9aacfd5388870da0d7
Abdul-Jabbar Bozdar <git@bozdar.im>
2025-12-22 23:35:25 +0500

    first day logged using readline module

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 console.clear();
+console.log(`======= MyDo To-Do CLI App =======\n`);
+let rlSync = require('readline-sync');
 let date = new Date();
-console.log(`Here is your to-do list for today ${date.getDate()}.`);
+let tomorrow = `
+Here is your to-do list for ${date.getDate()+1}-${date.getMonth()}-${date.getFullYear()}.\n`;
+
+let userSelectDay = rlSync.questionInt(`Enter 1 for today.\nEnter 2 for tomorrow.\nEnter 3 for all previous lists.\n\n`);
+
+if (userSelectDay === 2) {
+  console.log(`${tomorrow}`);
+  console.log(`
+    1. Do coding-dojo problems
+    2. Arithmetic Class 7 and 8
+    3. Read Spraul's Problem Solving book
+    4. Read Polya's Analogy
+    5. Improve MyDo App
+  `
+  );
+}
