c5b97d8321f4a8eb787da2ded84be5f510445f7e
Abdul-Jabbar Bozdar <git@bozdar.im>
2025-12-23 07:39:00 +0500

    User can view both lists for today and tomorrow

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ console.clear();
 console.log(`======= MyDo To-Do CLI App =======\n`);
 let rlSync = require('readline-sync');
 let date = new Date();
+let today = `
+Here is your to-do list for today ${date.getDate()}-${date.getMonth()}-${date.getFullYear()}.\n`;
 let tomorrow = `
 Here is your to-do list for ${date.getDate()+1}-${date.getMonth()}-${date.getFullYear()}.\n`;
 
+
+
 let userSelectDay = rlSync.questionInt(`Enter 1 for today.\nEnter 2 for tomorrow.\nEnter 3 for all previous lists.\n\n`);
 
-if (userSelectDay === 2) {
-  console.log(`${tomorrow}`);
+if (userSelectDay === 1) {
+  console.log(`${today}`);
   console.log(`
     1. Do coding-dojo problems
     2. Arithmetic Class 7 and 8
@@ -17,4 +21,9 @@ if (userSelectDay === 2) {
     5. Improve MyDo App
   `
   );
+} else if (userSelectDay === 2) {
+  console.log(`${tomorrow}`);
+  console.log(`
+    No entry yet.
+    `);
 }
